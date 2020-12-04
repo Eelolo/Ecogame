30692556572f9ea283963950aeeac7632834298e
Eelolo <Eelolo@yandex.ru>
2020-12-04 10:20:37 +0300

    login function

--- ecogame/auth.py
+++ ecogame/auth.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, request
+from flask import (Blueprint, g, request, session)
+from werkzeug.security import check_password_hash
 from ecogame.db import get_db
-from ecogame.db_operations import new_user
+from random import randint
+from ecogame.db_operations import (
+    new_user, add_credits, get_user_credits, get_user_items
+)
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -24,6 +28,45 @@ def register():
 
         if error is None:
             new_user(username, password)
-            return {'Registration': 'Succes'}
+            return {'Registration': 'Success'}
+
+        return {'Error': error}
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        if g.user is not None:
+            return {'Message': 'Already logged in.'}
+
+        username = request.json['username']
+        password = request.json['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM users WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['user_id']
+
+            bonuses = [450, 1350, 3100, 4800, 6300]
+            bonus = bonuses[randint(0, 4)]
+
+            add_credits(session['user_id'], bonus)
+
+            user_items = ', '.join(get_user_items(session['user_id']))
+
+            return {
+                "Bonus": "Received {} credits".format(bonus),
+                "Credits": get_user_credits(session['user_id']),
+                "Items": user_items
+            }
 
         return {'Error': error}
--- ecogame/db_operations.py
+++ ecogame/db_operations.py
@@ -2,6 +2,13 @@ from ecogame.db import get_db
 from werkzeug.security import generate_password_hash
 
 
+def fetchall_to_list(fetchall):
+    for idx in range(len(fetchall)):
+        fetchall[idx] = fetchall[idx][0]
+
+    return fetchall
+
+
 def new_user(username, password):
     db = get_db()
 
@@ -10,3 +17,42 @@ def new_user(username, password):
         (username, generate_password_hash(password))
     )
     db.commit()
+
+
+def get_user_credits(user_id):
+    db = get_db()
+
+    kredits = db.execute(
+        'SELECT credits FROM users WHERE user_id = ?', (user_id,)
+    ).fetchone()[0]
+    return kredits
+
+
+def add_credits(user_id, integer):
+    db = get_db()
+
+    db.execute(
+        'UPDATE users SET credits = ? WHERE user_id = ?',
+        (get_user_credits(user_id) + integer, user_id)
+    )
+    db.commit()
+
+
+def get_user_items(user_id):
+    db = get_db()
+
+    items_id = db.execute(
+        'SELECT item_id FROM user_items WHERE user_id = ?', (user_id,)
+    ).fetchall()
+
+    items_id = fetchall_to_list(items_id)
+
+    user_items = []
+    for idx in items_id:
+        user_items.append(db.execute(
+            'SELECT name FROM items WHERE item_id = ?', (idx,)
+        ).fetchone())
+
+    user_items = fetchall_to_list(user_items)
+
+    return user_items
