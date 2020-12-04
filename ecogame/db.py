844f2bf0362fbceeeb472c9f2f3ef96f1bc210b5
Eelolo <Eelolo@yandex.ru>
2020-12-04 05:32:37 +0300

    reset-db function

--- ecogame/db.py
+++ ecogame/db.py
@@ -1,7 +1,10 @@
 import sqlite3
 
+import click
 from flask import current_app
 from flask import g
+from flask.cli import with_appcontext
+import csv
 
 
 def get_db():
@@ -20,3 +23,31 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def reset_db():
+    db = get_db()
+
+    with current_app.open_resource('scheme.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+    with open('ecogame/items.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            db.execute(
+                'INSERT INTO items (name, price) VALUES (?, ?)',
+                (row['name'], row['price'])
+            )
+    db.commit()
+
+
+@click.command('reset-db')
+@with_appcontext
+def reset_db_command():
+    reset_db()
+    click.echo('Database is resetted.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(reset_db_command)
