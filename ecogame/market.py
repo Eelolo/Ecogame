b18928532bd4fc43ffafbbb0623b4cf52c70cbb0
Eelolo <Eelolo@yandex.ru>
2020-12-04 14:58:48 +0300

    items function

--- ecogame/market.py
+++ ecogame/market.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, session
 from ecogame.auth import login_required
+from ecogame.db import get_db
 from ecogame.db_operations import get_user_credits, get_user_items
 
 
@@ -13,3 +14,19 @@ def info():
     user_items = get_user_items(session['user_id'])
 
     return {'Credits': kredits, 'Items': user_items}
+
+
+@bp.route('/')
+@login_required
+def items():
+    db = get_db()
+    user_items = dict(db.execute(
+        'SELECT name, price FROM items'
+    ).fetchall())
+    response = {}
+    items_list = []
+    for item in user_items:
+        items_list.append([item, user_items[item]])
+    for idx in range(len(items_list)):
+        response[idx+1] = items_list[idx]
+    return response
