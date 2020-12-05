0077000c34f8ab4747b2b6cc5b582b5151e46419
Eelolo <Eelolo@yandex.ru>
2020-12-05 07:55:38 +0300

    login function

--- client/client.py
+++ client/client.py
@@ -1,5 +1,8 @@
 import requests
 
+logged_in = False
+cookies = None
+
 
 def json_printer(json):
     for key in json:
@@ -10,10 +13,28 @@ def register():
     register_data = {
         "username": input('Username: '),
         "password": input('Password: ')
-    }
+        }
 
     response = requests.post('http://127.0.0.1:8000/auth/register', json=register_data)
     json_printer(response.json())
+
+
+def login(cookies, logged_in):
+    auth_data = {
+        "username": input('Username: '),
+        "password": input('Password: ')
+        }
+
+    if not logged_in:
+        response = requests.post('http://127.0.0.1:8000/auth/login', json=auth_data)
+        if 'Set-Cookie' in response.headers:
+            cookies = response.headers['Set-Cookie']
+    else:
+        response = requests.post('http://127.0.0.1:8000/auth/login', json=auth_data, cookies=cookies)
+
+    json_printer(response.json())
+
+    return cookies
 
 
 while True:
@@ -23,6 +44,14 @@ while True:
 
     if client_command == 'register':
         register()
+    elif client_command == 'login':
+        if logged_in:
+            login(cookies, logged_in)
+        else:
+            cookies = login(None, logged_in)
+            if cookies is not None:
+                cookies = {'session': cookies.split('=')[1].split(';')[0]}
+                logged_in = True
     elif client_command == 'exit':
         exit()
     else:
