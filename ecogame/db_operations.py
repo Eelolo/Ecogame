75e1abf102544b7629a49e0fb4d174c50d932929
Eelolo <Eelolo@yandex.ru>
2020-12-05 03:11:09 +0300

    sell_item function

--- ecogame/db_operations.py
+++ ecogame/db_operations.py
@@ -76,3 +76,27 @@ def add_item(user_id, item_id):
         (user_id, item_id)
     )
     db.commit()
+
+
+def delete_last_purchased_item(user_id, item_id):
+    db = get_db()
+
+    some_items = db.execute(
+        'SELECT id FROM user_items WHERE user_id = ? AND item_id = ?',
+        (user_id, item_id)
+    ).fetchall()
+    some_items = fetchall_to_list(some_items)
+
+    item_to_delete = 0
+    for idx in some_items:
+        if item_to_delete < idx:
+            item_to_delete = idx
+
+    if item_to_delete == 0:
+        return item_to_delete
+
+    db.execute(
+        'DELETE FROM user_items WHERE user_id = ? AND item_id = ? AND id = ?',
+        (user_id, item_id, item_to_delete)
+    )
+    db.commit()
--- ecogame/market.py
+++ ecogame/market.py
@@ -2,9 +2,8 @@ from flask import Blueprint, session
 from ecogame.auth import login_required
 from ecogame.db import get_db
 from ecogame.db_operations import (
-    get_user_credits, get_user_items, add_item, subtract_credits
+    get_user_credits, get_user_items, add_item, subtract_credits, add_credits, delete_last_purchased_item
 )
-
 
 bp = Blueprint('market', __name__, url_prefix='/market')
 
@@ -57,3 +56,23 @@ def buy_item(item_id):
             return {'message': 'Not enough credits.'}
 
     return {'message': 'Item purchased.'}
+
+
+@bp.route('/sell/<int:item_id>')
+@login_required
+def sell_item(item_id):
+    db = get_db()
+
+    try:
+        price = db.execute(
+            'SELECT price FROM items WHERE item_id = ?', (item_id,)
+        ).fetchone()[0]
+    except TypeError:
+        return 'Incorrect item_id.'
+
+    add_credits(session['user_id'], price)
+
+    if delete_last_purchased_item(session['user_id'], item_id) == 0:
+        return {'message': 'There is no such item.'}
+
+    return {'message': 'Item sold.'}
